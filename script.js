5fb1860409b8fd355adef6ffb4af6dd174cba6db
alishajatav <162378770+alishajatav@users.noreply.github.com>
2026-03-26 00:27:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const year =promt("please enter a year");
+function  dayOfAYear(year) {
+	if(year%4==0 && year%100==0){
+		console.log("366")
+	}
+	else if(year%40==0 ){
+		console.log("366")
+	}
+	else {
+		console.log("365")
+	}
+}
